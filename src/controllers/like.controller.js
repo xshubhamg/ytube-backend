104174808e5a1c4074df5caa93ed74f4930597c0
Shubham Giri <shubhammgiri3@gmail.com>
2026-09-17 09:33:53 +0530

    add like feature with video comment tweet toggles

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,133 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { Video } from "../models/video.model.js";
+import { Comment } from "../models/comment.model.js";
+import { Tweet } from "../models/tweet.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) throw new ApiError(400, "Invalid videoId");
+
+  const video = await Video.findById(videoId);
+  if (!video) throw new ApiError(404, "Video not found");
+
+  const existingLike = await Like.findOne({
+    video: videoId,
+    likedBy: req.user?._id,
+  });
+
+  if (existingLike) {
+    await Like.findByIdAndDelete(existingLike._id);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { isLiked: false }, "Video unliked"));
+  }
+
+  await Like.create({ video: videoId, likedBy: req.user?._id });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { isLiked: true }, "Video liked"));
+});
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+
+  if (!isValidObjectId(commentId))
+    throw new ApiError(400, "Invalid commentId");
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) throw new ApiError(404, "Comment not found");
+
+  const existingLike = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  if (existingLike) {
+    await Like.findByIdAndDelete(existingLike._id);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { isLiked: false }, "Comment unliked"));
+  }
+
+  await Like.create({ comment: commentId, likedBy: req.user?._id });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { isLiked: true }, "Comment liked"));
+});
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+
+  if (!isValidObjectId(tweetId)) throw new ApiError(400, "Invalid tweetId");
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) throw new ApiError(404, "Tweet not found");
+
+  const existingLike = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user?._id,
+  });
+
+  if (existingLike) {
+    await Like.findByIdAndDelete(existingLike._id);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { isLiked: false }, "Tweet unliked"));
+  }
+
+  await Like.create({ tweet: tweetId, likedBy: req.user?._id });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { isLiked: true }, "Tweet liked"));
+});
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+  const likedVideos = await Like.aggregate([
+    {
+      $match: {
+        likedBy: new mongoose.Types.ObjectId(req.user?._id),
+        video: { $exists: true, $ne: null },
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "video",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                { $project: { fullname: 1, username: 1, avatar: 1 } },
+              ],
+            },
+          },
+          { $addFields: { owner: { $first: "$owner" } } },
+        ],
+      },
+    },
+    { $addFields: { video: { $first: "$video" } } },
+    { $sort: { createdAt: -1 } },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, likedVideos, "Liked videos fetched successfully")
+    );
+});
+
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos };
---
+++ src/routes/like.route.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  getLikedVideos,
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+} from "../controllers/like.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const likeRouter = Router();
+
+likeRouter.use(verifyJWT);
+
+likeRouter.route("/toggle/v/:videoId").post(toggleVideoLike);
+likeRouter.route("/toggle/c/:commentId").post(toggleCommentLike);
+likeRouter.route("/toggle/t/:tweetId").post(toggleTweetLike);
+likeRouter.route("/videos").get(getLikedVideos);
+
+export default likeRouter;
